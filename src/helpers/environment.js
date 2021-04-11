16dc298e691e96aebd4605a18610fef40847ccd6
GPMcKelvey <gpmckelvey@gmail.com>
2021-04-11 16:47:36 -0400

    third try for heroku

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,14 +1,10 @@
 let APIURL = '';
 
 switch (window.location.hostname) {
-    //this is the local host name of your react app
-    case 'localhost' || '127.0.0.1' :
-        //this is the local host name of your api
+        case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3000';
         break;
-    //this is the deployed react application
     case 'gpm-dailyplanner-client.herokuapp.com':
-        //this is the full url of your deployed api
         APIURL = 'https://gpm-dailyplanner-server.herokuapp.com'
 }
 

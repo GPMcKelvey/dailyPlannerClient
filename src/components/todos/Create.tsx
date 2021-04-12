9b09f4676de31d2236b04242769b232ecba2d7f5
GPMcKelvey <gpmckelvey@gmail.com>
2021-04-12 11:51:29 -0400

    finished todos bug fix

--- src/components/todos/Create.tsx
+++ src/components/todos/Create.tsx
@@ -70,7 +70,7 @@ export default class TodosCreate extends Component<AcceptedProps, CreateState> {
                 <Box display='flex' justifyContent='center' id='createTodoDiv'>
                     <Box m={1}>
                     <label>New Task: </label>
-                    <Input id='task' name='task' type='text' onChange={this.inputHandler}></Input>
+                    <Input id='task' name='task' type='text' onChange={this.inputHandler} value={this.state.task}></Input>
                     </Box>
                 <Box m={1}>
                     <ColorButton onClick={this.createFetch}>Add Task</ColorButton></Box>
